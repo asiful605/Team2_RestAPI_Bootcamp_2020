905dee1a176f7684be80a147d366ca2cbd74ea17
abidur1999 <abidur1999@gmail.com>
2020-07-03 01:35:30 -0400

    update2

--- Twitter1/src/test/java/testtweet/TestTweetAPIClient.java
+++ Twitter1/src/test/java/testtweet/TestTweetAPIClient.java
@@ -1,5 +1,7 @@
 package testtweet;
 
+import base.TwitterAPIClient;
+import com.github.scribejava.core.model.Response;
 import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -26,5 +28,46 @@ public class TestTweetAPIClient {
         String actualTweet=response.extract().body().path("text");
         Assert.assertEquals(tweet,actualTweet);
     }
+    protected TwitterAPIClient tweetsClient;
+    @BeforeClass
+    public  void setUp(){
+        this.tweetsClient = new TwitterAPIClient();
+    }
+    @Test
+    public void testUserCanTweetSuccessfully(){
+        Response respone = tweetsClient.sentTweet("Tweeting from Queen,newyork");
+        respone.getBody().prettyPrint();
+        int statusCode = respone.getStatusCode();
+        String statusLine=respone.getStatusLine();
+        Assert.assertEquals(statusCode,200);
+        Assert.assertEquals(statusLine,"HTTP/1.1 200 OK");
 
+    }
+    @Test
+    public void testUserCannnotTweetDuplicateTweet(){
+        Response respone = tweetsClient.sentTweet("Hey!This is Abidur" + UUID.randomUUID());
+        respone.getBody().prettyPrint();
+        int statusCode = respone.getStatusCode();
+        String statusLine=respone.getStatusLine();
+        Assert.assertEquals(statusCode,200);
+        Assert.assertEquals(statusLine,"HTTP/1.1 200 OK");
+    }
+    @Test
+    public void testUserCanSearchProfileSuccessfully(){
+        Response response= tweetsClient.searchProfile("tsbristy111");
+        response.getBody().prettyPrint();
+        int statusCode= response.getStatusCode();
+        String statusLine=response.getStatusLine();
+        Assert.assertEquals(statusCode,200);
+        Assert.assertEquals(statusLine,"HTTP/1.1 200 OK");
+    }
+    @Test
+    public void testUser2CanSearchProfileSuccessfully(){
+        Response response= tweetsClient.searchProfile("asif_");
+        response.getBody().prettyPrint();
+        int statusCode= response.getStatusCode();
+        String statusLine=response.getStatusLine();
+        Assert.assertEquals(statusCode,200);
+        Assert.assertEquals(statusLine,"HTTP/1.1 200 OK");
+    }
 }
